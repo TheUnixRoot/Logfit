860cac6f4cfd41a45a13f34ce7b1534b463e6b20
Juan Pedro Dominguez Berdun <juanp@linux-w80s.Home>
2018-10-14 01:16:52 +0200

    without unused files

--- source/cpp/Body.h
+++ source/cpp/Body.h
@@ -22,9 +22,8 @@ int RandomNumber () { return (std::rand()%NUM_RAND); }
  * **************************************************************************/
 class Body{
     // TODO: Fill this class with logic and methods
-private:
-    const int vsize = 10;
 public:
+    const int vsize = 10;
     buffer_f Adevice;
     buffer_f Bdevice;
     buffer_f Cdevice;
@@ -41,38 +40,18 @@ public:
     }
 
 	void OperatorGPU(tbb::flow::opencl_node<type_gpu> *gpuNode, t_index indexes) {
-        flow::interface10::input_port<0>(*gpuNode).try_put(indexes);
-		flow::interface10::input_port<1>(*gpuNode).try_put(Adevice);
-		flow::interface10::input_port<2>(*gpuNode).try_put(Bdevice);
-		flow::interface10::input_port<3>(*gpuNode).try_put(Cdevice);
-		cout << "\033[0;33m" << "GPU computing from: " << indexes.begin << " to: " << indexes.end << "\033[0m" << endl;
-	}
-
-	void getBackObjectFromGPU(int begin, int end, cl_event *event) {
-
-		//tick_count t0 = tick_count::now();
-
-		//fprintf(stderr, "GetBack: %d-%d\n",begin,end);
-		error = clEnqueueReadBuffer(command_queue, d_bodies, CL_NON_BLOCKING, begin * sizeof(OctTreeLeafNode),
-			sizeof(OctTreeLeafNode) * (end-begin), &bodies[begin], 0, NULL, NULL);
-
-		if (error != CL_SUCCESS) {
-			fprintf(stderr, "Failed copying  d_bodies to host!\n");
-			exit(0);
-		}
-		/*if(error == CL_SUCCESS){
-			fprintf(stderr, "GetBack success\n");
-		}*/
-
-		//tick_count t1 = tick_count::now();
-		//memory_time += (t1 - t0).seconds() * 1000; // in order to use milliseconds
+//        flow::interface10::input_port<0>(*gpuNode).try_put(indexes);
+//		flow::interface10::input_port<1>(*gpuNode).try_put(Adevice);
+//		flow::interface10::input_port<2>(*gpuNode).try_put(Bdevice);
+//		flow::interface10::input_port<3>(*gpuNode).try_put(Cdevice);
+		std::cout << "\033[0;33m" << "GPU computing from: " << indexes.begin << " to: " << indexes.end << "\033[0m" << std::endl;
 	}
 
 	void OperatorCPU(int begin, int end) {
 		for (int i = begin; i < end; i++) {
             Chost[i] = Ahost[i] + Bhost[i];
         }
-        cout << "\033[0;33m" << "CPU computing from: " << begin << " to: " << end << "\033[0m" << endl;
+        std::cout << "\033[0;33m" << "CPU computing from: " << begin << " to: " << end << "\033[0m" << std::endl;
 	}
 
 };
