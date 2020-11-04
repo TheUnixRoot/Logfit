d3869b473d4e11bf3ad154c0c66fbf848a22a0e3
Juan Pedro Dominguez Berdun <juanpe.domber@gmail.com>
2020-11-04 18:54:47 +0100

    Fixing issue with vsize

--- Graph/source/cpp/Implementations/Bodies/TestOneApiBody.h
+++ Graph/source/cpp/Implementations/Bodies/TestOneApiBody.h
@@ -23,9 +23,9 @@ public:
         std::cout << gpu_queue.get_device().get_info<info::device::name>() << std::endl;
         auto context = gpu_queue.get_context();
         auto device = gpu_queue.get_device();
-        A = (float*) malloc_shared(vsize * sizeof(float), device, context);
-        B = (float*) malloc_shared(vsize * sizeof(float), device, context);
-        C = (float*) malloc_shared(vsize * sizeof(float), device, context);
+        A = (float*) malloc_host(vsize * sizeof(float), context);
+        B = (float*) malloc_host(vsize * sizeof(float), context);
+        C = (float*) malloc_host(vsize * sizeof(float), context);
         std::generate(A, A + vsize, [] { return 1.0; });
         std::generate(B, B + vsize, [] { return 1.0; });
         std::generate(C, C + vsize, [] { return 0.0; });
@@ -37,21 +37,23 @@ public:
         }
     }
 
-    void ShowCallback() {
-        for (int i = 0; i < vsize; i++) {
-            std::cout << i << ": " << A[i] << " + " << B[i] << " = " << C[i] << std::endl;
-        }
-    }
-
     void OperatorGPU(int begin, int end) {
         using namespace cl::sycl;
+        size_t range_size = end - begin;
         gpu_queue.submit([&](handler& handler){
-            handler.parallel_for(range<1>{vsize}, [=](id<1> id){
-                auto idx = id[0];
+            handler.parallel_for(range<1>{range_size}, [=](id<1> id){
+                auto idx = id[0] + begin;
+                if (idx >= end) return;
                 C[idx] = A[idx] + B[idx];
             });
         });
         gpu_queue.wait();
+    }
+
+    void ShowCallback() {
+        for (int i = 0; i < vsize; i++) {
+            std::cout << i << ": " << A[i] << " + " << B[i] << " = " << C[i] << std::endl;
+        }
     }
 
     int GetVsize(){
