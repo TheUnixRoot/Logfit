4b92d28c845e006963bfd1f5941a7c3f2e0b1468
juan.dominguez <juan.dominguez@perfectchannel.com>
2021-02-06 12:03:29 +0100

    Latest commit. Cleanup

--- oneAPI2021/docs/RunBarnes.sh
+++
@@ -1,5 +0,0 @@
-!/bin/bash
-
-/home/juanp/logfit/Graph/cmake-build-debug/Logfit -f /home/juanp/logfit/Graph/data/runA.in -c 0 -g 1 -r 0.5 -k ComputeForce -o /home/juanp/logfit/Graph/source/cl/kernel.cl > ./BarnesWithoutCPU.txt
-/home/juanp/logfit/Graph/cmake-build-debug/Logfit -f /home/juanp/logfit/Graph/data/runA.in -c 2 -g 0 -r 0.5 -k ComputeForce -o /home/juanp/logfit/Graph/source/cl/kernel.cl > ./BarnesWithoutGPU.txt
-/home/juanp/logfit/Graph/cmake-build-debug/Logfit -f /home/juanp/logfit/Graph/data/runA.in -c 2 -g 1 -r 0.5 -k ComputeForce -o /home/juanp/logfit/Graph/source/cl/kernel.cl > ./BarnesWithGPU.txt
